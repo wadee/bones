90c9bc2796525a0362527a34a0c0f500239a67a8
zijing2 <415940392@qq.com>
2013-11-14 10:48:46 +0800

    bones_skull

---
+++ src/controller/Activity/GameStart/BOGameStartActivity.java
@@ -0,0 +1,22 @@
+package controller.Activity.GameStart;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BOGameStartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gamestart);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_bogame_start, menu);
+		return true;
+	}
+
+}
