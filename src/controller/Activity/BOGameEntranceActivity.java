319f96ac7941b41a8db58ec59a80cebcccfb8f3e
zijing2 <415940392@qq.com>
2013-11-19 17:26:05 +0800

    第一页各种按钮的事件

--- src/controller/Activity/BOGameEntranceActivity.java
+++ src/controller/Activity/BOGameEntranceActivity.java
@@ -1,15 +1,21 @@
 package controller.Activity;
 
+import javax.security.auth.PrivateCredentialPermission;
+
 import global.BOGlobalUserinfo;
+import android.R.anim;
 import android.R.string;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import controller.BOActivityAbstract;
 import controller.Activity.R;
@@ -32,6 +38,10 @@ public class BOGameEntranceActivity extends BOActivityAbstract {
 	
 	public BODrUser druser;
 	
+	private Button btn_start;
+	
+	private Button btn_quit;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,7 +59,13 @@ public class BOGameEntranceActivity extends BOActivityAbstract {
 		//将名字放到全局变量中
 		BOGlobalUserinfo.setUserName(name);
 		
+		//获取开始按钮
+		btn_start = (Button) findViewById(R.id.app_start);
+		btn_start.setOnClickListener(new StartButtonOnClickListener());
 		
+		//获取退出按钮
+		btn_quit = (Button) findViewById(R.id.app_quit);
+		btn_quit.setOnClickListener(new QuitButtonOnClickListener());
 		
 	}
 	
@@ -60,7 +76,7 @@ public class BOGameEntranceActivity extends BOActivityAbstract {
 		 
 		 builder.setTitle("请输入姓名");
 		 builder.setView(signname);
-		 builder.setPositiveButton("确定", new OnClickListener() {
+		 builder.setPositiveButton("确定", new android.content.DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				String sign_name = signname.getText().toString();
@@ -80,4 +96,35 @@ public class BOGameEntranceActivity extends BOActivityAbstract {
 	} 
 
 
+	//点击开始按钮时的事件
+	class StartButtonOnClickListener implements android.view.View.OnClickListener{
+		@Override
+		public void onClick(View v) {
+			//打开wifi
+			
+			//跳转到connect页
+			Intent intent = new Intent();
+			intent.setClass(BOGameEntranceActivity.this,BOGameConnectActivity.class);
+			BOGameEntranceActivity.this.startActivity(intent);
+			
+		}
+	}
+	
+	//点击退出按钮事件
+	class QuitButtonOnClickListener implements android.view.View.OnClickListener{
+
+		@Override
+		public void onClick(View arg0) {
+			//让用户回到首页
+			Intent intent = new Intent(Intent.ACTION_MAIN);  
+            intent.addCategory(Intent.CATEGORY_HOME);  
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);  
+            startActivity(intent);
+            //杀死游戏进程
+            android.os.Process.killProcess(android.os.Process.myPid());
+		}
+		
+	}
+
+	
 }
